cdbb5f39dc10348c133496d992f3e83c874a8195
RimuruG <907300985@qq.com>
2021-12-03 09:28:19 +0800

    update

--- 2109/springboot_demo1/src/main/java/com/jt/SpringbootDemo1Application.java
+++ 2109/springboot_demo1/src/main/java/com/jt/SpringbootDemo1Application.java
@@ -9,6 +9,7 @@ public class SpringbootDemo1Application {
     public static void main(String[] args) {
         //springBoot方式管理的spring容器
         System.out.println("最终项目启动");
+        System.out.println("啥也不是");
         SpringApplication.run(SpringbootDemo1Application.class, args);
     }
 
