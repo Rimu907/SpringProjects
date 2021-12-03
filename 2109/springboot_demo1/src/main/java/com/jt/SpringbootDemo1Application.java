2c9789a61c13d44ac3477281272770dd52c76190
RimuruG <907300985@qq.com>
2021-12-03 09:13:38 +0800

    up

---
+++ 2109/springboot_demo1/src/main/java/com/jt/SpringbootDemo1Application.java
@@ -0,0 +1,15 @@
+package com.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo1Application {
+
+    public static void main(String[] args) {
+        //springBoot方式管理的spring容器
+        System.out.println("项目启动");
+        SpringApplication.run(SpringbootDemo1Application.class, args);
+    }
+
+}
